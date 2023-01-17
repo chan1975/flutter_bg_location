cd4b302172fe2ad70bec0aab1cd5e156029232f9
chan1975 <cesarsaa1991@gmail.com>
2023-01-17 09:21:33 -0500

    csa - v2

--- lib/main.dart
+++ lib/main.dart
@@ -1,46 +1,209 @@
-import 'package:flutter/material.dart';
-import 'change_settings.dart';
-import 'listen_location.dart';
-import 'get_location.dart';
+/**
+* flutter_background_geolocation Hello World
+* https://github.com/transistorsoft/flutter_background_geolocation
+*/
 
-void main() => runApp(const MyApp());
+import 'package:flutter/material.dart';
+import 'package:flutter_background_geolocation/flutter_background_geolocation.dart'
+    as bg;
+import 'package:cron/cron.dart';
+
+////
+// For pretty-printing location JSON.  Not a requirement of flutter_background_geolocation
+//
+import 'dart:convert';
+
+JsonEncoder encoder = new JsonEncoder.withIndent("     ");
+//
+////
+void main() => runApp(new MyApp());
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Location',
-      theme: ThemeData(
+    return new MaterialApp(
+      title: 'BackgroundGeolocation Demo',
+      theme: new ThemeData(
         primarySwatch: Colors.amber,
       ),
-      home: const MyHomePage(title: 'Flutter Location Demo'),
+      home: new MyHomePage(),
     );
   }
 }
 
-class MyHomePage extends StatelessWidget {
-  const MyHomePage({super.key, this.title});
-  final String? title;
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key? key}) : super(key: key);
+
+  final String title = "BackgroundGeolocation Demo";
+
+  @override
+  _MyHomePageState createState() => new _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  bool? _isMoving;
+  bool? _enabled;
+  String? _motionActivity;
+  String? _odometer;
+  String? _content;
+  Cron cron = Cron();
+
+  @override
+  void initState() {
+    _isMoving = false;
+    _enabled = false;
+    _content = '';
+    _motionActivity = 'UNKNOWN';
+    _odometer = '0';
+
+    // 1.  Listen to events (See docs for all 12 available events).
+    bg.BackgroundGeolocation.onLocation(_onLocation);
+    bg.BackgroundGeolocation.onMotionChange(_onMotionChange);
+    bg.BackgroundGeolocation.onActivityChange(_onActivityChange);
+    bg.BackgroundGeolocation.onProviderChange(_onProviderChange);
+    bg.BackgroundGeolocation.onConnectivityChange(_onConnectivityChange);
+
+    // 2.  Configure the plugin
+    bg.BackgroundGeolocation.ready(bg.Config(
+            desiredAccuracy: bg.Config.DESIRED_ACCURACY_HIGH,
+            distanceFilter: 10.0,
+            stopOnTerminate: false,
+            startOnBoot: true,
+            debug: true,
+            logLevel: bg.Config.LOG_LEVEL_VERBOSE,
+            reset: true))
+        .then((bg.State state) {
+      setState(() {
+        _enabled = state.enabled;
+        _isMoving = state.isMoving;
+      });
+    });
+  }
+
+  void _onClickEnable(enabled) {
+    if (enabled) {
+      bg.BackgroundGeolocation.start().then((bg.State state) {
+        print('[start] success $state');
+        setState(() {
+          _enabled = state.enabled;
+          _isMoving = state.isMoving;
+        });
+      });
+    } else {
+      bg.BackgroundGeolocation.stop().then((bg.State state) {
+        print('[stop] success: $state');
+        // Reset odometer.
+        bg.BackgroundGeolocation.setOdometer(0.0);
+
+        setState(() {
+          _odometer = '0.0';
+          _enabled = state.enabled;
+          _isMoving = state.isMoving;
+        });
+      });
+    }
+  }
+
+  // Manually toggle the tracking state:  moving vs stationary
+  void _onClickChangePace() {
+    setState(() {
+      _isMoving = !_isMoving!;
+    });
+    print("[onClickChangePace] -> $_isMoving");
+
+    bg.BackgroundGeolocation.changePace(_isMoving!).then((bool isMoving) {
+      print('[changePace] success $isMoving');
+    }).catchError((e) {
+      print('[changePace] ERROR: ' + e.code.toString());
+    });
+  }
+
+  // Manually fetch the current position.
+  void _onClickGetCurrentPosition() {
+    cron = Cron();
+    cron.schedule(Schedule.parse('*/3 * * * *'), () async {
+      print('Cron job started');
+      bg.BackgroundGeolocation.getCurrentPosition(
+              persist: true, // <-- do not persist this location
+              desiredAccuracy: 0, // <-- desire best possible accuracy
+              timeout: 20000, // <-- wait 20s before giving up.
+              samples: 3 // <-- sample 3 location before selecting best.
+              )
+          .then((bg.Location location) {
+        print('[getCurrentPosition] - $location');
+      }).catchError((error) {
+        print('[getCurrentPosition] ERROR: $error');
+      });
+    });
+  }
+
+  ////
+  // Event handlers
+  //
+
+  void _onLocation(bg.Location location) {
+    print('[location] - $location');
+
+    String odometerKM = (location.odometer / 1000.0).toStringAsFixed(1);
+
+    setState(() {
+      _content = encoder.convert(location.toMap());
+      _odometer = odometerKM;
+    });
+  }
+
+  void _onMotionChange(bg.Location location) {
+    print('[motionchange] - $location');
+  }
+
+  void _onActivityChange(bg.ActivityChangeEvent event) {
+    print('[activitychange] - $event');
+    setState(() {
+      _motionActivity = event.activity;
+    });
+  }
+
+  void _onProviderChange(bg.ProviderChangeEvent event) {
+    print('$event');
+
+    setState(() {
+      _content = encoder.convert(event.toMap());
+    });
+  }
+
+  void _onConnectivityChange(bg.ConnectivityChangeEvent event) {
+    print('$event');
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(title!),
-      ),
-      body: SingleChildScrollView(
-        child: Column(
-          children: const <Widget>[
-            SizedBox(height: 16),
-            GetLocationWidget(),
-            Divider(height: 32),
-            ListenLocationWidget(),
-          ],
-        ),
-      ),
+      appBar:
+          AppBar(title: const Text('Background Geolocation'), actions: <Widget>[
+        Switch(value: _enabled!, onChanged: _onClickEnable),
+      ]),
+      body: SingleChildScrollView(child: Text('$_content')),
+      bottomNavigationBar: BottomAppBar(
+          child: Container(
+              padding: const EdgeInsets.only(left: 5.0, right: 5.0),
+              child: Row(
+                  mainAxisSize: MainAxisSize.max,
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    IconButton(
+                      icon: Icon(Icons.gps_fixed),
+                      onPressed: _onClickGetCurrentPosition,
+                    ),
+                    Text('$_motionActivity · $_odometer km'),
+                    MaterialButton(
+                        minWidth: 50.0,
+                        child: Icon(
+                            (_isMoving!) ? Icons.pause : Icons.play_arrow,
+                            color: Colors.white),
+                        color: (_isMoving!) ? Colors.red : Colors.green,
+                        onPressed: _onClickChangePace)
+                  ]))),
     );
   }
 }
